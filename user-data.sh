088b98331f5e4c0cf9adb8ccbab4ab22b3484fe4
Korakrit6509650203 <korakrit.pon@dome.tu.ac.th>
2024-12-02 01:07:03 +0700

    fix ci, add user-data.sh, add cd

---
+++ user-data.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Update the package manager and install Docker
+sudo yum update -y
+sudo yum install -y docker
+
+# Start Docker and enable it to start on boot
+sudo service docker start
+sudo systemctl enable docker
+
+# Add the ec2-user to the Docker group (so you can run Docker commands without sudo)
+sudo usermod -aG docker ec2-user
+newgrp docker
+
+echo "Starting Docker pull and run..."
+
+sudo docker pull korakrit/cs360_frontend_image_test:latest
+sudo docker run -p 3000:3000 --name cs360_frontend_container korakrit/cs360_frontend_image_test:latest
+
+#sudo docker pull korakrit/cs360_backend_image_test:latest
+#sudo docker run -p 1337:1337 --name cs360_backend_container korakrit/cs360_backend_image_test:latest
+
+echo "Docker containers are running."
