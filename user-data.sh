f7908d9c174498f64c3d14f8b809beeb67cfb214
Korakrit6509650203 <korakrit.pon@dome.tu.ac.th>
2024-12-02 19:15:50 +0700

    fix CICD.yml

--- user-data.sh
+++ user-data.sh
@@ -14,9 +14,9 @@ newgrp docker
 echo "Starting Docker pull and run..."
 
 sudo docker pull korakrit/cs360_frontend_image_test:latest
-sudo docker run -p 3000:3000 --name cs360_frontend_container korakrit/cs360_frontend_image_test:latest
+sudo docker run -d -p 3000:3000 --name cs360_frontend_container korakrit/cs360_frontend_image_test:latest
 
-#sudo docker pull korakrit/cs360_backend_image_test:latest
-#sudo docker run -p 1337:1337 --name cs360_backend_container korakrit/cs360_backend_image_test:latest
+sudo docker pull korakrit/cs360_backend_image_test:latest
+sudo docker run -p 1337:1337 --name cs360_backend_container korakrit/cs360_backend_image_test:latest
 
 echo "Docker containers are running."
