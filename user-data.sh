0fb35418a460eef338a7b9756fa445f5cf295f56
Korakrit6509650203 <korakrit.pon@dome.tu.ac.th>
2024-12-02 21:51:28 +0700

    fix user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -15,6 +15,14 @@ echo "Starting Docker pull and run..."
 
 sudo docker pull korakrit/cs360_frontend_image_test:latest
 sudo docker run -d -p 3000:3000 --name cs360_frontend_container korakrit/cs360_frontend_image_test:latest
+sudo docker exec cs360_frontend_container sh -c "
+PUBLIC_IP=\$(curl -s ifconfig.me) &&
+echo \"REACT_APP_STRIPE_APP_KEY=\$(openssl rand -hex 32)\" > /usr/src/app/.env &&
+echo \"REACT_APP_DEV_URL=http://\$PUBLIC_IP:1337\" >> /usr/src/app/.env &&
+echo \"REACT_APP_STRIPE_PUBLISHABLE_KEY=\$(openssl rand -hex 32)\" >> /usr/src/app/.env
+echo "Exiting script..."
+exit 0"
+sudo docker exec cs360_frontend_container npm run build
 
 sudo docker pull korakrit/cs360_backend_image_test:latest
 sudo docker run -p 1337:1337 --name cs360_backend_container korakrit/cs360_backend_image_test:latest
