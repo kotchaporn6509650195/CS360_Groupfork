727effd272f451211da6a10e5a81144b31841b50
jiratya_6509650278 <jiratya.tha@dome.tu.ac.th>
2024-12-04 23:19:37 +0700

    fix bug

--- user-data.sh
+++ user-data.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#Update package lists
+sudo yum update -y
+
+#Install Nginx
+sudo yum install nginx -y
+
+#Start and enable Nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
 # Update the package manager and install Docker
 sudo yum update -y
 sudo yum install -y docker
