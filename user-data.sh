0e160f5ce6bab038fa2a0c1d9bdb4396faa0b5df
Kotchaporn <kotchaporn.mee@dome.tu.ac.th>
2024-12-08 12:17:15 +0700

    T

--- user-data.sh
+++ user-data.sh
@@ -28,4 +28,4 @@ sudo docker exec cs360_frontend_container npm run build
 sudo docker pull korakrit/cs360_backend_image_test:latest
 sudo docker run -p 1337:1337 --name cs360_backend_container korakrit/cs360_backend_image_test:latest
 
-#"Docker containers are running."
+"Docker containers are running."
